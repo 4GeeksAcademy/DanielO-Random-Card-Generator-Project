e5648082b0be261e85845629d2ee7e9fa8b13f82
oluwatobioyewole <oluwatobioyewole84@gmail.com>
2024-10-07 13:11:49 +0000

    all necessary random elements are represeneted accordingly

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♣", "♥", "♠"];
+  let value = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "k",
+    "A"
+  ];
+
+  let chosenSuit = getrandomElement(suit);
+  let chosenValue = getrandomElement(value);
+  let leftSuit = document.querySelector(".left-suit");
+  let middleNumber = document.querySelector(".middle-number");
+  let rightSuit = document.querySelector(".right-suit");
+  leftSuit.innerHTML = chosenSuit;
+  middleNumber.innerHTML = chosenValue;
+  rightSuit.innerHTML = chosenSuit;
+  console.log(chosenSuit, chosenValue, chosenSuit);
 };
+
+function getrandomElement(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
